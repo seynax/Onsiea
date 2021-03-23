8723b4ebe7fd2800e86aae18f35906f39d7dd8f3
Seynax <seyronax@gmail.com>
2021-03-23 12:02:29 +0100

    BufferHelper improvements

--- utils/fr/seynax/onsiea/utils/BufferHelper.java
+++ utils/fr/seynax/onsiea/utils/BufferHelper.java
@@ -1,17 +1,38 @@
 package fr.seynax.onsiea.utils;
 
+import java.nio.ByteBuffer;
+import java.nio.DoubleBuffer;
 import java.nio.FloatBuffer;
+import java.nio.LongBuffer;
+import java.nio.ShortBuffer;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.system.MemoryUtil;
 
 public class BufferHelper
 {
-	public final static FloatBuffer toFloatBUffer(final float... floatsIn)
+	public final static ByteBuffer createByteBuffer(final byte... contentsIn)
 	{
-		final var floatBuffer = MemoryUtil.memAllocFloat(floatsIn.length);
+		return BufferUtils.createByteBuffer(contentsIn.length).put(contentsIn).flip();
+	}
 
-		floatBuffer.put(floatsIn).flip();
+	public final static ShortBuffer createShortBuffer(final short... contentsIn)
+	{
+		return MemoryUtil.memAllocShort(contentsIn.length).put(contentsIn).flip();
+	}
 
-		return floatBuffer;
+	public final static FloatBuffer createFloatBuffer(final float... contentsIn)
+	{
+		return MemoryUtil.memAllocFloat(contentsIn.length).put(contentsIn).flip();
+	}
+
+	public final static DoubleBuffer createDoubleBuffer(final double... contentsIn)
+	{
+		return MemoryUtil.memAllocDouble(contentsIn.length).put(contentsIn).flip();
+	}
+
+	public final static LongBuffer createFloatBuffer(final long... contentsIn)
+	{
+		return MemoryUtil.memAllocLong(contentsIn.length).put(contentsIn).flip();
 	}
 }
