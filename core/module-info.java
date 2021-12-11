b588dd65d6ba11ce2db97b722f5a45c4f2299014
alex zaros <seyronax@gmail.com>
2021-12-11 15:32:53 +0100

    Module info creation
    Creation of the module-info.java in core folder root, to manage engine services and prerequisites

---
+++ core/module-info.java
@@ -0,0 +1,17 @@
+module fr.seynax.onsiea
+{
+	requires org.lwjgl;
+	requires org.joml;
+	requires org.lwjgl.glfw;
+	requires org.lwjgl.opengl;
+	requires org.lwjgl.openal;
+	requires org.lwjgl.stb;
+	requires org.lwjgl.nanovg;
+	requires org.lwjgl.vulkan;
+	requires org.lwjgl.vma;
+	requires org.lwjgl.shaderc;
+
+	requires java.desktop;
+	requires static java.management;
+	requires static jdk.management;
+}
